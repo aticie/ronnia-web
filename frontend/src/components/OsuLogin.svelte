df8d4545d6c2ac4e58327bbdf6bad3cbf14dc8dc
Efehan <efehanatici@gmail.com>
2021-03-28 11:39:43 +0300

    add components

---
+++ frontend/src/components/OsuLogin.svelte
@@ -0,0 +1,22 @@
+<button on:click={_ => {window.location.href = '/osu_authorize'}}>
+</button>
+
+<style>
+  button{
+    transition: ease-in-out 0.2s;
+    background-image: url('https://osu.ppy.sh/assets/images/osu-logo-white.59d385da.svg'), url('https://osu.ppy.sh/assets/images/osu-logo-triangles.b1e57c37.svg');
+    background-size: auto;
+    background-color: transparent;
+    border-radius: 100%;
+    border:none;
+    outline:none;
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    margin-top: 25px;
+  }
+  button:hover{
+    filter: brightness(0.5);
+  }
+</style>
+
---
+++ frontend/src/components/TwitchLogin.svelte
@@ -0,0 +1,23 @@
+<button
+  on:click={(_) => {
+    window.location.href = "/twitch_authorize";
+  }}
+>
+</button>
+
+<style>
+  button{
+    transition: ease-in-out 0.2s;
+    background-image: url('/images/twitch-logo.svg');
+    background-color: transparent;
+    border:none;
+    outline:none;
+    width: 90px;
+    height: 90px;
+    margin: 15px;
+    margin-top: 30px;
+  }
+  button:hover{
+    filter: brightness(0.5);
+  }
+</style>
