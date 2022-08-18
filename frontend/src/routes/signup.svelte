fd9407bae0a1d67df1d539d12dba8ff65bdc0368
aticie <efehanatici@gmail.com>
2022-08-18 22:25:04 +0200

    Actually make sign-up work

--- frontend/src/routes/signup.svelte
+++ frontend/src/routes/signup.svelte
@@ -1,5 +1,6 @@
 <script>
   import Cookies from "js-cookie";
+  import {toOsuAuth, toTwitchAuth} from "../utils";
 
   let signupCookie = Cookies.get("signup");
 </script>
@@ -8,12 +9,12 @@
   <p class="text-center font-semibold">Complete your sign-up to Ronnia with</p>
 
   {#if signupCookie == "twitch"}
-    <button class="button b-with-icon w-1/2">
+    <button class="button b-with-icon w-1/2" on:click={toTwitchAuth}>
       <img src="/public/osu!LogoWhite.svg" alt="osu icon" />
       osu! 
     </button>
   {:else}
-    <button class="button b-with-icon w-1/2">
+    <button class="button b-with-icon w-1/2" on:click={toOsuAuth}>
       <img src="/public/twitchGlitchWhite.svg" alt="twitch icon" />
       Twitch
     </button>
