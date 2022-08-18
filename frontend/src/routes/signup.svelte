ca24c032d0f1ae9743ac97c62f2fbf660e7794d9
aticie <efehanatici@gmail.com>
2022-08-18 22:47:36 +0200

    Fix buttons again -.-

--- frontend/src/routes/signup.svelte
+++ frontend/src/routes/signup.svelte
@@ -9,12 +9,12 @@
   <p class="text-center font-semibold">Complete your sign-up to Ronnia with</p>
 
   {#if signupCookie == "twitch"}
-    <button class="button b-with-icon w-1/2" on:click={toTwitchAuth}>
+    <button class="button b-with-icon w-1/2" on:click={toOsuAuth}>
       <img src="/public/osu!LogoWhite.svg" alt="osu icon" />
       osu! 
     </button>
   {:else}
-    <button class="button b-with-icon w-1/2" on:click={toOsuAuth}>
+    <button class="button b-with-icon w-1/2" on:click={toTwitchAuth}>
       <img src="/public/twitchGlitchWhite.svg" alt="twitch icon" />
       Twitch
     </button>
