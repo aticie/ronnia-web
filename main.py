ef6cf4c5a09d1a9368aa17f355d0e539f043248c
aticie <efehanatici@gmail.com>
2023-04-06 14:35:30 +0200

    change host to environment variable

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("backend.api:create_api", host="0.0.0.0", port=int(os.getenv('PUBLISH_PORT')), reload=True,
+    uvicorn.run("backend.api:create_api", host=os.getenv("PUBLISH_HOST"), port=int(os.getenv('PUBLISH_PORT')), reload=True,
                 reload_dirs=['./backend', './frontend/src'], factory=True)
