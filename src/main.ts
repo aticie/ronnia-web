8242188a1b76416d23cd1e33484fe3db75086216
metkm <metinkorkmaz417@gmail.com>
2023-04-28 20:11:11 +0300

    update the name of css file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from "./routes";
 
-import './assets/style.css'
+import './assets/main.css'
 
 createApp(App).use(Router).mount('#app')
