80a27318b1cf491c735a72e31991b30960ea4cb2
metkm <metinkorkmaz417@gmail.com>
2023-05-04 19:30:15 +0300

    add axios, cleanup excluded component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 import Development from "./components/TheDevRouter.vue";
 import TheToaster from "./components/TheToaster.vue";
 import BaseSuspense from "./components/base/BaseSuspense.vue";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE;
 
 const isDev = import.meta.env.DEV;
 </script>
--- src/components/settings/SettingExcluded.vue
+++ src/components/settings/SettingExcluded.vue
@@ -1,86 +1,65 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { useVModel } from "@vueuse/core";
+import { useBus } from "../../composables";
+import axios from "axios";
+
+import SettingBase from './SettingBase.vue';
 import BaseButton from "../base/BaseButton.vue";
-import SettingBase from "./SettingBase.vue";
-import IconAdd from "../icons/IconAdd.vue";
 import IconDelete from "../icons/IconDelete.vue";
-import {ref} from "vue";
-import {useFetch, useVModel} from "@vueuse/core";
-import {useBus} from "../../composables";
+import IconAdd from "../icons/IconAdd.vue";
 
 const props = defineProps<{
-  modelValue: string[];
+  modelValue: string[]
 }>();
-const emit = defineEmits(["update:modelValue"]);
-const ids = useVModel(props, "modelValue", emit);
+const emit = defineEmits<{
+  (event: "update:modelValue", payload: string[]): void
+}>();
+
 const bus = useBus();
+const users = useVModel(props, "modelValue", emit);
+const user = ref();
 
-const userId = ref("");
+const response = await axios.get<string[]>("/user/exclude");
+users.value = response.data;
 
-const excludeBaseUrl = `${import.meta.env.VITE_API_BASE}/user/exclude`;
+const addExcluded = async () => {
+  if (!user.value) return;
 
-const {data} = await useFetch(
-    excludeBaseUrl,
-    {credentials: "include"}
-).json();
-ids.value = data.value;
-
-const addUser = async () => {
-  if (!userId.value) return;
-  if (userId.value in ids.value) {
+  if (users.value.includes(user.value)) {
     bus.emit({
       title: "Error while adding user!",
-      description: "User already exists!",
-    });
+      description: "User already exists."
+    })
+
     return;
   }
 
-  let user = userId.value
-  const params = new URLSearchParams({
-    excluded_user: user
+  await axios.post<string[]>("/user/exclude", {}, {
+    params: {
+      excluded_user: user.value
+    }
   });
-  const addUserUrl = `${excludeBaseUrl}?` + params.toString()
-  const {statusCode, error, data} = await useFetch(
-      addUserUrl,
-      {
-        credentials: "include",
-      }
-  ).post().text();
 
-  if (statusCode.value !== 200) {
-    bus.emit({
-      title: "Error while adding user!",
-      description: error.value,
-    });
-  } else {
-    bus.emit({title: `Added ${data.value} to excluded users!`});
-    ids.value.unshift(user);
-    userId.value = "";
-  }
-};
+  users.value.unshift(user.value);
 
-const removeUser = async (index: number) => {
-  let user = ids.value[index];
-  const params = new URLSearchParams({
-    excluded_user: user
+  bus.emit({
+    title: `Added ${user.value} to excluded users!`
+  })
+}
+
+const removeExcluded = async (user: string) => {
+  await axios.delete("/user/exclude", {
+    params: {
+      excluded_user: user
+    }
   });
-  const url = `${import.meta.env.VITE_API_BASE}/user/exclude?` + params.toString()
-  const {statusCode, error, data} = await useFetch(
-      url,
-      {
-        credentials: "include",
-      }
-  ).delete().text();
 
-  if (statusCode.value !== 200) {
-    bus.emit({
-      title: "Error while removing user!",
-      description: error.value,
-    });
-  } else {
-    bus.emit({title: `Removed ${data.value} from excluded users!`});
-    ids.value.splice(index, 1);
-  }
-};
+  bus.emit({ title: `Removed ${user} from excluded users!` });
+
+  let index = users.value.findIndex(x => x === user);
+  users.value.splice(index, 1);
+}
 </script>
 
 <template>
@@ -91,13 +70,13 @@ const removeUser = async (index: number) => {
 
     <div class="flex flex-wrap gap-2 w-full">
       <input
-          v-model="userId"
-          @keyup.enter="addUser"
-          class="bg-neutral-950 rounded p-2 grow"
+        v-model="user"
+        @keyup.enter="addExcluded"
+        class="bg-neutral-950 rounded p-2 grow"
       />
-      <BaseButton @click="addUser">
+      <BaseButton @click="addExcluded">
         <template #icon>
-          <IconAdd/>
+          <IconAdd />
         </template>
         <p>Add</p>
       </BaseButton>
@@ -107,23 +86,23 @@ const removeUser = async (index: number) => {
       Users
     </p>
     <ul
-        class="bg-neutral-950 max-h-60 overflow-y-auto w-full relative rounded divide-y divide-neutral-800 overflow-hidden"
+      class="bg-neutral-950 max-h-60 overflow-y-auto w-full relative rounded divide-y divide-neutral-800 overflow-hidden"
     >
       <p
-          v-if="ids.length === 0"
-          class="p-2 text-center text-neutral-700 text-sm"
+        v-if="users.length === 0"
+        class="p-2 text-center text-neutral-700 text-sm"
       >
         You can add users that should be excluded here!
       </p>
       <li
-          v-for="(id, index) in ids"
-          :key="id"
-          class="flex items-center justify-between p-1 group"
+        v-for="user in users"
+        :key="user"
+        class="flex items-center justify-between p-1 group"
       >
-        <p class="ml-2">{{ id }}</p>
-        <BaseButton @click="removeUser(index)">
+        <p class="ml-2">{{ user }}</p>
+        <BaseButton @click="removeExcluded(user)">
           <template #icon>
-            <IconDelete/>
+            <IconDelete />
           </template>
           <p>Delete</p>
         </BaseButton>
