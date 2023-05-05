70eb59f62a10d74e6250034f4ebdbd8b40d8a0d6
metkm <metinkorkmaz417@gmail.com>
2023-05-05 20:01:00 +0300

    finish

--- src/components/TheLiveStreams.vue
+++ src/components/TheLiveStreams.vue
@@ -10,34 +10,25 @@ const domain = window.location.hostname;
 </script>
 
 <template>
-  <div class="lg:absolute w-full flex items-end inset-0 overflow-hidden">
-    <!-- <div class="lg:surface flex flex-col overflow-hidden lg:w-[150%]"> -->
-      <!-- <p class="lg:text-xl text-neutral-800 text-center font-bold">
-        Some Cool Streamers that use Ronnia!
-      </p> -->
-
-      <div class="h-1/2">
-        <div
-          class="surface mx-auto mask grid grid-cols-5 gap-4"
-        >
-          <!-- class="lg:mask flex flex-wrap justify-center lg:justify-normal gap-4" -->
-          <img
-            v-for="streamer in response.data"
-            :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-            class="w-80 aspect-video object-cover"
-          />
-          <img
-            v-for="streamer in response.data"
-            :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-            class="w-80 aspect-video object-cover"
-          />
-          <img
-            v-for="streamer in response.data"
-            :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-            class="w-80 aspect-video object-cover"
-          />
-        </div>
-
-      </div>
+  <div class="lg:absolute inset-0 flex justify-end items-end overflow-hidden">
+    <div
+      class="surface mask grid grid-cols-4 gap-4 max-w-7xl h-[70%] overflow-y-auto"
+    >
+      <img
+        v-for="streamer in response.data"
+        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
+        class="w-full aspect-video object-cover"
+      />
+      <img
+        v-for="streamer in response.data"
+        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
+        class="w-80 aspect-video object-cover"
+      />
+      <img
+        v-for="streamer in response.data"
+        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
+        class="w-80 aspect-video object-cover"
+      />
+    </div>
   </div>
 </template>
