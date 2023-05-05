e7fec6ce6ef53c756ae008fabbc90a64d7c2a9bc
metkm <metinkorkmaz417@gmail.com>
2023-05-05 20:17:23 +0300

    add text

--- src/components/TheLiveStreams.vue
+++ src/components/TheLiveStreams.vue
@@ -1,34 +1,44 @@
 <script setup lang="ts">
 import axios from "axios";
+import { ref, onMounted, onUnmounted } from "vue";
+import { useInfiniteScroll } from "@vueuse/core";
 
-const response = await axios.get("/live", {
+const response = await axios.get<string[]>("/live", {
   withCredentials: false,
   params: { limit: 1000 },
 });
 
-const domain = window.location.hostname;
+const streamers = ref(response.data);
+const target = ref<HTMLElement | null>(null);
+
+let interval: number;
+onMounted(() => {
+  interval = setInterval(() => {
+    target.value?.scrollTo({ top: target.value.scrollTop + 1, behavior: "smooth" });
+  }, 40)
+})
+
+onUnmounted(() => clearInterval(interval))
+
+useInfiniteScroll(target, () => {
+  streamers.value.push(...streamers.value);
+}, { distance: 50 })
 </script>
 
 <template>
   <div class="lg:absolute inset-0 flex justify-end items-end overflow-hidden">
-    <div
-      class="surface mask grid grid-cols-4 gap-4 max-w-7xl h-[70%] overflow-y-auto"
-    >
-      <img
-        v-for="streamer in response.data"
-        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-        class="w-full aspect-video object-cover"
-      />
-      <img
-        v-for="streamer in response.data"
-        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-        class="w-80 aspect-video object-cover"
-      />
-      <img
-        v-for="streamer in response.data"
-        :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
-        class="w-80 aspect-video object-cover"
-      />
+    <div class="surface max-w-7xl h-[70%]">
+      <p class="text-neutral-800 font-bold text-center">Some cool streamers that use Ronnia!</p>
+      <div
+        ref="target"
+        class="mask grid grid-cols-4 gap-4 overflow-y-auto h-full"
+      >
+        <img
+          v-for="streamer in streamers"
+          :src="`https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer}.jpg`"
+          class="w-full aspect-video object-cover hover:scale-110 transition-transform"
+        />
+      </div>
     </div>
   </div>
 </template>
